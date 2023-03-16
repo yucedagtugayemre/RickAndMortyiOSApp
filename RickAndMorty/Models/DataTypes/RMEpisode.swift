f0c341377405c10efb096b11dd362d4f1803b445
yucedagEmre <emrethedev@gmail.com>
2023-03-16 14:04:56 +0300

    Create episode card & detail

--- RickAndMorty/Models/DataTypes/RMEpisode.swift
+++ RickAndMorty/Models/DataTypes/RMEpisode.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct RMEpisode: Codable {
-    let id: String
+struct RMEpisode: Codable, RMEpisodeDataRender {
+    let id: Int
     let name: String
     let air_date: String
     let episode: String
     let characters: [String]
     let url: String
-    let create: String
+    let created: String
 }
--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -7,9 +7,63 @@
 
 import Foundation
 
+protocol RMEpisodeDataRender {
+    var name: String {get}
+    var air_date: String {get}
+    var episode: String {get}
+}
+
 final class RMCharacterEpisodeCollectionViewCellViewModel {
     private let episodeDataUrl: URL?
+    
+    private var isFetching = false
+    
+    private var dataBlock: ((RMEpisodeDataRender) -> Void)?
+    
+    private var episode: RMEpisode? {
+        didSet {
+            guard let model = episode else {
+                return
+            }
+            self.dataBlock?(model)
+        }
+    }
+    
+    // MARK: - Public
+    
+    public func registerForData(_ block: @escaping (RMEpisodeDataRender) -> Void) {
+        self.dataBlock = block
+    }
+    
+    // MARK: - Init
+    
     init(episodeDataUrl: URL?) {
         self.episodeDataUrl = episodeDataUrl
+    }
+    
+    public func fetchEpisode() {
+        guard !isFetching else {
+            if let model = episode {
+                self.dataBlock?(model)
+            }
+            return
+        }
+        
+        guard let url = episodeDataUrl, let rmRequest = RMRequest(url: url) else {
+            return
+        }
+        isFetching = true
+        RMService.shared.execute(rmRequest,
+                                 expecting: RMEpisode.self) {[weak self] result in
+            guard let storngSelf = self else {return}
+            switch result {
+            case .success(let model):
+                DispatchQueue.main.async {
+                    storngSelf.episode = model
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+            }
+        }
     }
 }
