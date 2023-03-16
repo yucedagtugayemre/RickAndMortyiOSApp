4d5def84313cb7eccdcc1def06212274bdb54509
yucedagEmre <emrethedev@gmail.com>
2023-03-16 09:27:50 +0300

    character detail page info card done

--- RickAndMorty/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
@@ -12,8 +12,8 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     
     private let valueLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Earht"
         label.font = .systemFont(ofSize: 22, weight: .light)
         return label
     }()
@@ -21,7 +21,7 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Location"
+        
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 20,weight: .medium)
         return label
@@ -30,7 +30,7 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     private let iconImageView: UIImageView = {
         let icon = UIImageView()
         icon.translatesAutoresizingMaskIntoConstraints = false
-        icon.image = UIImage(systemName: "globe.americas")
+        
         icon.contentMode = .scaleAspectFit
         return icon
     }()
@@ -78,21 +78,28 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
             
             valueLabel.leftAnchor.constraint(equalTo:  iconImageView.rightAnchor, constant:  10),
             valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor,constant:  -10),
-            valueLabel.topAnchor.constraint(equalTo:  contentView.topAnchor, constant:  36),
-            valueLabel.heightAnchor.constraint(equalToConstant: 30)
- 
+            valueLabel.topAnchor.constraint(equalTo:  contentView.topAnchor),
+            valueLabel.bottomAnchor.constraint(equalTo: titleContainerView.topAnchor)
+            
             
         ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        //        valueLabel.text = nil
-        //        titleLabel.text = nil
-        //        iconImageView.image = nil
+        valueLabel.text = nil
+        titleLabel.text = nil
+        iconImageView.image = nil
+        iconImageView.tintColor = .label
+        titleLabel.textColor = .label
+        
     }
     
     public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
-        
+        titleLabel.text = viewModel.title
+        valueLabel.text = viewModel.displayValue
+        iconImageView.image = viewModel.iconImage
+        iconImageView.tintColor = viewModel.tintColor
+        titleLabel.textColor = viewModel.tintColor
     }
 }
